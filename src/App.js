c0a6fc4e09ee442139c4a48e23f63ee36a1bf37d
jorgelnc <jorgenavacolina@gmail.com>
2023-10-21 20:59:34 -0500

    react project freecodecamp testimonios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import { Fragment } from 'react';
 import './App.css';
+import Testimonio from './components/Testimonio';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='contenedor-principal'>
+        <h1>Here is what our alumni say about freeCodeCamp:</h1>
+          <Testimonio
+          nombre='Shawn Wang'
+          pais='Singapore'
+          imagen='shawn'
+          cargo='Software Engineer'
+          empresa='Amazon'
+          testimonio={<Fragment>Its scary to change careers. I only gained confidence that I could code 
+          by working through the hundreds of hours of free lessons on freeCodeCamp.
+          Within a year I had a six-figure job as a Software Engineer. <strong>freeCodeCamp changed my life</strong>.</Fragment>}/>
+          <Testimonio
+          nombre='Sarah Chima'
+          pais='Nigeria'
+          imagen='sarah'
+          cargo='Software Engineer'
+          empresa='ChatDesk'
+          testimonio={<Fragment><strong>freeCodeCamp was the gateway to my career</strong> as a software developer. 
+          The well-structured curriculum took my coding knowledge
+          from a total beginner level to a very confident level.
+          It was everything I needed to land my first dev job at an amazing company.</Fragment>}/>
+          <Testimonio
+          nombre='Emma Bostian'
+          pais='Sweden'
+          imagen='emma'
+          cargo='Software Engineer'
+          empresa='Spotify'
+          testimonio={<Fragment>I've always struggled with learning JavaScript. 
+          Ive taken many courses but freeCodeCamps course was the one which stuck.
+          Studying JavaScript as well as data structures and algorithms on <strong>freeCodeCamp
+          gave me the skills</strong>and confidence I needed to land my dream job as a software engineer at Spotify.</Fragment>}/>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
